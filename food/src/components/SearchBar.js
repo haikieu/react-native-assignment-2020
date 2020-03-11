be702804071b42b26f678aba5518627392583707
Hai Kieu <haikieu2907@gmail.com>
2020-03-10 23:07:41 -0700

    Add Search Screen, and search bar

---
+++ food/src/components/SearchBar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { View, Text, StyleSheet, TextInput} from 'react-native';
+import { Feather } from '@expo/vector-icons';
+
+const SearchBar = () => {
+    return (
+        <View style={styles.background}>
+            <Feather name='search' style={styles.iconStyle}/>
+            <TextInput placeholder='search' style={styles.inputStyle}/>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        
+    },
+    background: {
+        backgroundColor: '#F0EEEE',
+        height: 50,
+        borderRadius: 5,
+        margin: 20,
+        padding: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+
+    },
+    inputStyle: {
+        flex:1,
+        fontSize: 18
+    },
+    iconStyle: {
+        fontSize: 30,
+        alignSelf: 'center',
+        marginHorizontal: 15
+    }
+});
+
+export default SearchBar;
---
+++ food/src/screens/SearchScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import SearchBar from '../components/SearchBar';
+
+const SearchScreen = () => {
+    return (
+        <View>
+            <SearchBar></SearchBar>
+            <Text>Search screen</Text>
+        </View>
+    );
+}
+
+
+
+export default SearchScreen;
